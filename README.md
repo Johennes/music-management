cf5f7effbd56a07e07f0b8dd381cb9ae73f11a72
Johannes Marbach <n0-0ne+gitlab@mailbox.org>
2018-12-20 14:38:06 +0100

    Add add-replay-gain.sh script

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Music Management
 
 A set of tools and configurations I use for managing my digital collection of music
+
+## add-replay-gain.sh
+
+A Bash script for batch-computing and writing ReplayGain tags. Only FLAC files are supported currently.
+
+```
+Usage: add-replay-gain directory
+
+Recursively walks over the specified directory and its
+subdirectories and adds ReplayGain tags to FLACs in every
+folder. If all files in a given folder are already tagged,
+the folder is skipped (unless -f is specified).
+
+  -f    Force recalculation and overwrite existing tags
+  -h    Show this message and exit
+```
---
+++ add-replay-gain.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Usage Info
+
+function usage {
+    echo "Usage: add-replay-gain directory"
+    echo ""
+    echo "Recursively walks over the specified directory and its"
+    echo "subdirectories and adds ReplayGain tags to FLACs in every"
+    echo "folder. If all files in a given folder are already tagged,"
+    echo "the folder is skipped (unless -f is specified)."
+    echo ""
+    echo "  -f    Force recalculation and overwrite existing tags"
+    echo "  -h    Show this message and exit"
+}
+
+# CLI Option Handling
+
+FORCE=false
+
+while getopts ":hf" OPT; do
+    case "${OPT}" in
+        f)
+            FORCE=true
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [[ -z "$1" ]]; then
+    echo "Error: No directory specified"
+    echo ""
+    usage
+    exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+    echo "Error: No such directory $1"
+    echo ""
+    usage
+    exit 1
+fi
+
+# File Processing
+
+while read -r DIR; do
+    if [[ $(ls "${DIR}" | grep -c \\.flac) -lt 1 ]]; then
+        continue
+    fi
+
+    echo "Processing ${DIR}..."
+
+    if ! ${FORCE}; then
+        echo "Checking existing ReplayGain tags..."
+        UNTAGGED=false
+        while read -r FILE; do
+            TRACK=$(metaflac --show-tag=REPLAYGAIN_TRACK_GAIN "${FILE}")
+            ALBUM=$(metaflac --show-tag=REPLAYGAIN_ALBUM_GAIN "${FILE}")
+            if [[ -z  "${TRACK}" || -z "${ALBUM}" ]]; then
+                UNTAGGED=true
+                break
+            fi
+        done < <(find "${DIR}" -maxdepth 1 -name "*.flac")
+
+        if ! ${UNTAGGED}; then
+            echo "All files already tagged, skipping directory"
+            continue
+        fi
+
+        echo "Untagged files found, calculating ReplayGain values..."
+    else
+        echo "Force-recalculating ReplayGain values..."
+    fi
+
+    metaflac --add-replay-gain "${DIR}"/*.flac
+
+    while read -r FILE; do
+        echo "Computed ReplayGain values for ${FILE}:"
+        metaflac --show-tag=REPLAYGAIN_TRACK_GAIN "${FILE}"
+        metaflac --show-tag=REPLAYGAIN_ALBUM_GAIN "${FILE}"
+    done < <(find "${DIR}" -maxdepth 1 -name "*.flac")
+done < <(find "$1" -type d)
